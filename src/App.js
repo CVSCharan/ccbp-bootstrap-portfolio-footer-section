ccb7b3eeb265a1567bd673d91f44d90edfb57d14
CVSCharan <charan.cvs@gmail.com>
2022-08-08 19:26:35 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import myimg from './myimg.jpeg';
 
 function App() {
   return (
@@ -7,9 +8,9 @@ function App() {
             <div className="row">
                 <div className="col-12 col-md-3">
                     <div className="card1 d-flex flex-row">
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/portfolio-about-me-img.png" className="img1" alt ="img1"/>
+                        <img src={myimg} className="img1" alt ="img1"/>
                         <div>
-                            <h1 className="h1">Jenny</h1>
+                            <h1 className="h1">CVS CHARAN</h1>
                             <p className="p1">Frontend Developer</p>
                         </div>
                     </div>
@@ -19,7 +20,7 @@ function App() {
                     <h1 className="h2">Phone No.</h1>
                     <div className="card2 d-flex flex-row">
                         <i className="fas fa-phone-alt img2"></i>
-                        <p className="p2">9876543210</p>
+                        <p className="p2">+91 7337525111</p>
                     </div>
                 </div>
 
@@ -27,7 +28,7 @@ function App() {
                     <h1 className="h2">E-mail</h1>
                     <div className="card2 d-flex flex-row">
                         <i className="fas fa-envelope img2"></i>
-                        <p className="p2">jenny3@gmail.com</p>
+                        <p className="p2">charan.cvs@gmail.com</p>
                     </div>
                 </div>
 
@@ -35,7 +36,7 @@ function App() {
                     <h1 className="h2">Address</h1>
                     <div className="card2 d-flex flex-row">
                         <i className="fas fa-map-marked-alt img2"></i>
-                        <p className="p2">17 Ayur Vigyan Nagar, New Delhi, India.</p>
+                        <p className="p2"> Hyderabad, India.</p>
                     </div>
                 </div>
 
@@ -44,7 +45,7 @@ function App() {
         <div className="ftcontainer text-center">
             <p className="p3">
                 <i className="far fa-copyright img2"></i>
-                2020 by Jenny Wilson. Created with Bootstrap.
+                2020 by CVS CHARAN. Created with Bootstrap.
             </p>
         </div>
     </div>
